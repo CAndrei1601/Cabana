7b0baa13345da2f550afbab63ecc5cff35475373
AndreiSGOO7 <andrei@softgenius.com>
2023-04-01 13:23:10 +0300

    despre noi all devices

---
+++ pages/despre-noi.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    p="x-6 t-6 b-56px md:x-40px t-40px b-80px"
+    flex="~ col lg:row"
+    justify="lg:between"
+    gap="lg:20px 3xl:168px"
+    class="max-w-1500px"
+    m="lg:x-auto"
+  >
+    <picture class="lg:order-2">
+      <nuxt-source
+        src="/images/image_despre-noi_Tablet.jpg"
+        sizes="sm:650
+        md:850px lg:896px"
+        media="(min-width:600px)"
+        format="webp"
+        height="944"
+        width="944"
+      />
+      <nuxt-img
+        src="/images/image_despre-noi.jpg"
+        format="webp"
+        height="426"
+        width="342"
+        w="full"
+        h="full"
+        rounded="2xl"
+        class="image-radius-2 sm:min-h-480px lg:order-2"
+        object="lg:cover"
+      />
+    </picture>
+    <div class="lg:order-1">
+      <h2 p="t-56px b-6 sm:t-80px" text="center green100">
+        {{ $t("despreNoi") }}
+      </h2>
+      <div flex="~ col" gap="4" text="green100" class="max-w-700px" m="x-auto">
+        <p>{{ $t("cabanaEsteSituatain") }}</p>
+        <p>{{ $t("cabanaSeAflaLa") }}</p>
+        <p>{{ $t("muntiiCalimani") }}</p>
+      </div>
+      <div m="t-4 sm: t-40px" flex="~" justify="center" items="center">
+        <NuxtLink to="/galerie">
+          <button class="btn-secondary" w="140px">
+            {{ $t("veziPoze") }}
+          </button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
