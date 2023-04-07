24803676a0aa7b99c816e51adf4d08911e45e48c
AndreiSGOO7 <andrei@softgenius.com>
2023-04-07 16:53:44 +0300

    set cookie popup & add cookie & politica onf keys

---
+++ components/cookiesPopup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="isOpen" class="cookie-popup">
+    <p>
+      Acest site folosește <NuxtLink to="/cookies"><u>cookies.</u></NuxtLink>
+    </p>
+    <button @click="acceptCookies" class="secondary">Accept</button>
+  </div>
+</template>
+<style lang="scss">
+.cookie-popup {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 24px;
+  background-color: #f0ede4;
+}
+</style>
+<script setup>
+const isOpen = ref(false);
+
+const acceptCookies = () => {
+  isOpen.value = false;
+  localStorage.setItem("GDPR:accepted", "true");
+};
+
+const getGDPR = () => {
+  return localStorage.getItem("GDPR:accepted");
+};
+
+onBeforeMount(() => {
+  if (getGDPR() === "true") {
+    isOpen.value = false;
+  } else {
+    isOpen.value = true;
+  }
+});
+</script>
