8186a179d3bef70816d133868deae6f3d4deeeb6
AndreiSGOO7 <andrei@softgenius.com>
2023-10-27 19:44:24 +0300

    add transition to pages

--- pages/index.vue
+++ pages/index.vue
@@ -76,6 +76,18 @@
     </section>
   </div>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
+
 <script setup lang="ts">
 const showModal = ref(false);
 
